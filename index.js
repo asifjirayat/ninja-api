0d126ac32c3898a816f2672afb5430b724c0b619
asifjirayat <asifjirayat@gmail.com>
2019-12-31 17:09:25 +0530

    Completed API routes

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const routes = require('./routes/api');
 
 //setup express app
 const app = express();
 
+const MONGO_URI = 'mongodb://localhost:27017/ninjago';
+mongoose.connect(MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true}, (err) => {
+    if(err) {
+        console.log(err);
+    }
+});
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+
 app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    res.status(422).send({error: err.message});
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up.');
--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/ninjas', (req, res) => {
-    res.send({type: 'GET'});
+const Ninja = require('../models/ninja');
+
+
+router.get('/ninjas', (req, res, next) => {
+   var lat = parseFloat(req.query.lat);
+   var lng = parseFloat(req.query.lng);
+   Ninja.aggregate([
+       {
+           $geoNear: {
+               near: { type: 'Point', coordinates: [lng, lat] },
+               distanceField: 'dist.calculated',
+               maxDistance: 100000000000000000,
+               spherical: true
+           }
+       }
+   ]).then((ninjas) => {
+    res.send(ninjas);
+   }).catch(next);
 });
 
-router.post('/ninjas', (req, res) => {
-    res.send({type: 'POST'});
+router.post('/ninjas', (req, res, next) => {    
+   var ninja = new Ninja(req.body);
+   ninja.save().then((ninja) => {
+    res.send(ninja)
+   }).catch(next);
 });
 
-router.put('/ninjas/:id', (req, res) => {
-    res.send({type: 'PUT'});
+router.put('/ninjas/:id', (req, res, next) => {
+    var id = req.params.id;
+    Ninja.findByIdAndUpdate(id, req.body, {new: true}).then((ninja) => {        
+        res.send(ninja);
+    }).catch((next));
 });
 
-router.delete('/ninjas/:id', (req, res) => {
-    res.send({type: 'DELETE'});
+router.delete('/ninjas/:id', (req, res, next) => {
+    var id = req.params.id;
+    Ninja.findByIdAndDelete({
+        _id: id
+    }).then((ninja) => {
+        if(!ninja) {
+            return res.status(404).send();
+        }
+        res.send(ninja);
+    }).catch(next);
 });
 
 module.exports = router;
